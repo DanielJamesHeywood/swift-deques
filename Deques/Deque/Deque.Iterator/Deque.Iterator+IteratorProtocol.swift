0abb7a8d61f1f2086782e5b33662d6374afcb87e
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-04 17:14:39 +0100

    Implemented IteratorProtocol for Iterator.

--- Deques/Deque/Deque.Iterator/Deque.Iterator+IteratorProtocol.swift
+++ Deques/Deque/Deque.Iterator/Deque.Iterator+IteratorProtocol.swift
@@ -3,6 +3,12 @@ extension Deque.Iterator: IteratorProtocol {
     
     @inlinable
     public mutating func next() -> Element? {
-        fatalError()
+        guard _nextIndex < _baseStorage.count else {
+            return nil
+        }
+        defer {
+            _nextIndex += 1
+        }
+        return _baseStorage[_nextIndex]
     }
 }
