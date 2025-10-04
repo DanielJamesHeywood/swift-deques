f15366db9b7880b44abca3515410a94a6a12b63f
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-04 15:03:51 +0100

    Implemented Iterator.

--- Deques/Deque/Deque.Iterator/Deque.Iterator.swift
+++ Deques/Deque/Deque.Iterator/Deque.Iterator.swift
@@ -2,5 +2,17 @@
 extension Deque {
     
     @frozen
-    public struct Iterator {}
+    public struct Iterator {
+        
+        @usableFromInline
+        internal let _baseStorage: _Storage
+        
+        @usableFromInline
+        internal var _nextIndex = 0
+        
+        @inlinable
+        internal init(_base: Deque) {
+            _baseStorage = _base._storage
+        }
+    }
 }
